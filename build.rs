8a220ba66eab2efac8ba74f37b13754bda78ecf1
b1n <120295547+b1nhack@users.noreply.github.com>
2023-06-03 21:00:16 -0700

    Added requireAdministrator manifest

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::io::Write;
+
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#);
+
+    match res.compile() {
+        Err(e) => {
+            write!(std::io::stderr(), "{}", e).unwrap();
+            std::process::exit(1);
+        }
+        Ok(_) => ()
+    }
+}
